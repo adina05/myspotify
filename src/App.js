c8d9cad959cc81e90b0689eb34d0e950d8b751ca
Adina Bucur <adina_anaid@yahoo.com>
2020-05-01 18:23:03 +0300

    Update Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Route, Switch } from "react-router-dom";
 import './App.css';
+import LabeledInput from "./LabeledInput";
+
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Category from "./pages/Category";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to ="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to ="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to ="/login">Login</Link>
+                    </li>
+                </ul>
+            </nav>
+            </header>
+            <main>
+                <Switch>
+                <Route
+                    path="/"
+                    exact={true}
+                    render={ () => <div>Home page</div>}/>
+                <Route
+                    path="/about"
+                    component={About}/>
+                <Route
+                    path="/login"
+                    component={Login}/>
+                <Route
+                     path="/categories/:id"
+                     component={Category}/>
+                </Switch>
+            </main>
+        </div>
+    );
 }
 
 export default App;
