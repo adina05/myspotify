d1a5e27b1ef8e691ce5520b081d5f1c7a26e7e12
Adina Bucur <adina_anaid@yahoo.com>
2020-06-02 15:47:18 +0300

    Profile Name in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 // import { Navbar, Nav, Button, Form, FormControl,Container } from "react-bootstrap";
 import { Navbar, Nav } from "react-bootstrap";
 import SearchBar from "./SearchBar";
+import { checkAndReturnToken } from "../utils"
 
+ const Header=()=>{
 
-class Header extends React.Component {
-    render() {
+     const [user, setUser] = useState("");
+
+     useEffect(
+         () => {
+             setUserName()
+         });
+
+    const setUserName=async()=>{
+        if(checkAndReturnToken() !==null &&checkAndReturnToken() !==undefined) {
+            const result= await fetch("https://api.spotify.com/v1/me",
+                {
+                   method:"GET",
+                    headers:{
+                        Authorization: `Bearer ${checkAndReturnToken()}`
+                    }
+                }
+                )
+            const data = await result.json();
+            setUser(data.display_name);
+        }
+     };
+        const expiredSession=()=>{
+            if(user!==undefined){
+                return `Logged in as ${user}`
+            }else{
+                return <button><Nav.Link href="/login">Login</Nav.Link></button>
+            }
+        }
+
         return (
                 <Navbar expand="lg" bg="light">
+                    <div>
                     <SearchBar/>
+                    </div>
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="pages-links">
                         <Nav.Item>
@@ -21,7 +52,7 @@ class Header extends React.Component {
                             <Nav.Link href="/about">About</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="/login">Login</Nav.Link>
+                            {expiredSession()}
                         </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
@@ -29,8 +60,7 @@ class Header extends React.Component {
 
         );
 
-    }
-}
 
+}
 
 export default Header;
